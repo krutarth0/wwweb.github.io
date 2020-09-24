10a630e9fa42ba9e1818b776d3c433ac8429ba89
krutarth0 <krutarthzero@hotmail.com>
2020-09-24 17:21:32 +0530

    publish the wwweb

---
+++ precache-manifest.167de7e3a4be2e109f0c51902dfce597.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1639b47839c7987fe1d9410e561b9828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "017ecb50f3357f6ac261",
+    "url": "/static/css/main.6d2ebaf3.chunk.css"
+  },
+  {
+    "revision": "214f44b4f5a7f89bac7f",
+    "url": "/static/js/2.060ac084.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/static/js/2.060ac084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017ecb50f3357f6ac261",
+    "url": "/static/js/main.32522d91.chunk.js"
+  },
+  {
+    "revision": "cf089d07c8d222f462d0",
+    "url": "/static/js/runtime-main.cc7c6b02.js"
+  },
+  {
+    "revision": "71aa2fbda74584cdab2fe11ded1ec350",
+    "url": "/static/media/arrow-left.71aa2fbd.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "ccba939b0130e3652f06560b1661b2cc",
+    "url": "/static/media/img (1).ccba939b.jpg"
+  },
+  {
+    "revision": "2c61d2904a4c2bd2c07012da5e4f560c",
+    "url": "/static/media/img (2).2c61d290.jpg"
+  },
+  {
+    "revision": "f35fedf04f7be9493c48bcf206e8c31a",
+    "url": "/static/media/img (3).f35fedf0.jpg"
+  }
+]);
